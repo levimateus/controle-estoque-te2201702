28019af80d8b8b2f8f3b35fd7adea7f393850137
Mates Levi <levimateus2011@gmail.com>
2017-09-27 21:49:07 -0300

    Teste de usuário não cadastrado não consegue fazer login

--- controle-estoque/tests/Feature/UsuariosTest.php
+++ controle-estoque/tests/Feature/UsuariosTest.php
@@ -26,10 +26,14 @@ class UsuariosTest extends TestCase
     public function test_um_usuario_podera_fazer_login_no_sistema(){
         Artisan::call('migrate');
 
-        $usuario = new User;
-        $usuario->name = 'josé';
-        $usuario->email = 'josé@email.com';
-        $usuario->password = bcrypt('password');
+        User::create([
+            'name' => 'jose',
+            'email' => 'jose@email.com',
+            'admin' => 0,
+            'password' => bcrypt('password')
+        ]);
+
+        $this->assertDatabaseHas('users', ['email' => 'jose@email.com']);
 
         Session::start();
         $response = $this->call('POST', '/login', [
@@ -38,11 +42,25 @@ class UsuariosTest extends TestCase
             '_token' => csrf_token()
         ]);
         $this->assertEquals(302, $response->getStatusCode());
+        $response->assertRedirect('/home');
+    }
+
+    public function test_usuario_nao_cadastrado_nao_consegue_logar(){
+        Artisan::call('migrate');
+        Session::start();
+
+        $response = $this->call('POST', '/login', [
+            'email' => 'bad@email.com',
+            'password' => 'bad_password',
+            '_token' => csrf_token()
+        ]);
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertDatabaseMissing('users', ['email' => 'bad@email.com', 'password' => bcrypt('bad_password')]);
+        $response->assertRedirect('/');
     }
 
     public function test_usuario_administrador_pode_cadastrar_usuarios(){
-
-    	$this->assertTrue(true);
+        $this->assertTrue(true);
     }
 
     public function test_usuario_administrador_pode_desativar_acesso_de_usuarios(){
